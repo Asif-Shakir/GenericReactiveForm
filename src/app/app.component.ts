79f603506df673f5d36aedc368db0b2640ae7964
Asif-Shakir~ <meharasifshakir@gmail.com~>
2022-12-11 20:38:49 +0500

    design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,32 +13,38 @@ export class AppComponent {
   patchValues: any;
   formControls: FormControlOptions[] = [
     { inputType: this.inputType.InputHidden, controlName: 'id', labelName: 'id', value: (Math.floor(Math.random() * 1000)) },
-    { inputType: this.inputType.InputText, controlName: 'firstName', labelName: 'First Name', value:'', controlValidatros: [Validators.required] },
+    {
+      inputType: this.inputType.InputText, controlName: 'firstName', labelName: 'First Name', value: ''
+      , controlValidatros: [Validators.required], class: 'col-4'
+    },
     {
       inputType: this.inputType.InputText, controlName: 'lastName', labelName: 'Last Name',
-      controlValidatros: [Validators.required, Validators.maxLength(10), Validators.minLength(3)]
+      controlValidatros: [Validators.required, Validators.maxLength(10), Validators.minLength(3)], class: 'col-4'
     },
-    { inputType: this.inputType.InputText, controlName: 'phone', labelName: 'Phone', value: '03009477899', controlValidatros: [Validators.required]},
     {
-      inputType: this.inputType.InputSelect, controlName: 'users', labelName: 'Users', controlValidatros: [Validators.required],
-      optionValues: [{ text: 'Lahore', value: '1' }, { text: 'Karachi', value: '2' }, { text: 'Peshawar', value: '3' }],
+      inputType: this.inputType.InputText, controlName: 'phone', labelName: 'Phone', value: '03009477899',
+      controlValidatros: [Validators.required], class: 'col-4'
+    },
+    {
+      inputType: this.inputType.InputSelect, controlName: 'users', labelName: 'Users', controlValidatros: [Validators.required], class: 'col-4'
+      ,optionValues: [{ text: 'Lahore', value: '1' }, { text: 'Karachi', value: '2' }, { text: 'Peshawar', value: '3' }],
       dataUrl: 'https://jsonplaceholder.typicode.com/users', mapByKeys: { text: 'name', value:'id'}
     },
     {
-      inputType: this.inputType.InputSelect, controlName: 'posts', labelName: 'Posts', controlValidatros: [Validators.required]
+      inputType: this.inputType.InputSelect, controlName: 'posts', labelName: 'Posts', controlValidatros: [Validators.required],class: 'col-4'
       , dependsOn: 'users', dataUrl: 'https://jsonplaceholder.typicode.com/posts?userId=', mapByKeys: { text: 'title', value: 'id' }
     },
     {
-      inputType: this.inputType.InputSelect, controlName: 'comments', labelName: 'Comments', controlValidatros: [Validators.required]
+      inputType: this.inputType.InputSelect, controlName: 'comments', labelName: 'Comments', controlValidatros: [Validators.required], class: 'col-4'
       , dependsOn: 'posts', dataUrl: 'https://jsonplaceholder.typicode.com/comments?postId=', mapByKeys: { text: 'name', value: 'id' }
     },
-    { inputType: this.inputType.InputDate, controlName: 'startDate', labelName: 'Start Date'},
-    { inputType: this.inputType.InputCheckBox, controlName: 'isActive', labelName: 'Active' },
+    { inputType: this.inputType.InputDate, controlName: 'startDate', labelName: 'Start Date',class: 'col-3'},
     {
       inputType: this.inputType.InputRadio, controlName: 'gender', labelName: 'Gender', controlValidatros: [Validators.required],
-      optionValues: [{ text: 'Male', value: '1' }, { text: 'Female', value: '2' }],
+      optionValues: [{ text: 'Male', value: '1' }, { text: 'Female', value: '2' }],class: 'col-3'
     },
-    { inputType: this.inputType.InputFile, controlName: 'fileLogo', labelName: 'Logo' },
+    { inputType: this.inputType.InputFile, controlName: 'fileLogo', labelName: 'Logo',class: 'col-3' },
+    { inputType: this.inputType.InputCheckBox, controlName: 'isActive', labelName: 'Active' ,class: 'col-3'},
   ];
   ngOnInit() {
     this.getLocalStrogeData();
